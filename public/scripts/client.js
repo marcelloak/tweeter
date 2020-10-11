ac68ca46d86362832dc751db11a2880ec7cf66ab
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-10-11 13:14:43 -0600

    Moved document.ready event calls into helper functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,52 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-import { loadTweets, errorManager, composerCharCounter } from "./helpers.js";
+import { loadTweets, useComposeTweetButton, composerCharCounter, newTweetSubmit, useTopButton, alterButtons } from "./helpers.js";
 
 $(document).ready(function() {
   loadTweets();
 
-  //When new tweet is submitted, deal with submitted text
-  $("#new-tweet-form").submit(function(event) {
-    event.preventDefault();
-    const tweet = $('#new-tweet-form').serialize();
-    const contents = decodeURI(tweet.slice(5));
-    if(!errorManager(contents)) {
-      $("#new-tweet-form")[0].reset();
-      $.post('/tweets/', tweet)
-        .then(() => loadTweets());
-    }
-  });
+  // When compose tweet button is clicked, open/close compose tweet viewer
+  $("#write-tweet").click(useComposeTweetButton);
 
-  //When window is scrolled, deal with top button
-  window.onscroll = function() {
-    const pageOffset = document.documentElement.scrollTop || document.body.scrollTop;
-    if (pageOffset > 100) {
-      $("#top").show();
-      $("#write-tweet").hide();
-    } else {
-      $("#top").hide();
-      $("#write-tweet").show();
-    }
-  };
-
-  //When compose tweet button is clicked, open/close compose tweet viewer
-  $("#write-tweet").click(function() {
-    if ($(".new-tweet").is(':visible')) $(".new-tweet").slideUp(500);
-    else {
-      $(".new-tweet").slideDown(500);
-      $("#tweet-text").focus();
-    }
-  });
-
-  //When top button is clicked, go to top and open compose tweet viewer if closed
-  $("#top").click(function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    if (!$(".new-tweet").is(':visible')) $(".new-tweet").slideDown(500);
-    $("#tweet-text").focus();
-  });
-
-  //Alters character counter in new tweet box based on tweet text contents
+  // When text in new tweet box altered, change character counter
   $("#tweet-text").on("keyup", composerCharCounter);
+
+  // When new tweet is submitted, deal with submitted text
+  $("#new-tweet-form").submit(newTweetSubmit);
+
+  // When top button is clicked, go to top and open compose tweet viewer if closed
+  $("#top").click(useTopButton);
+
+  // When window is scrolled, deal with compose tweet and top buttons
+  window.onscroll = alterButtons;
 });
--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -35,7 +35,7 @@ const createTweetElement = function(tweet) {
   `);
 };
 
-//Renders all tweets given, creating html elements for all of them and appending them to the tweets section
+// Renders all tweets given, creating html elements for all of them and appending them to the tweets section
 const renderTweets = function(tweets) {
   tweets.reverse();
   $('#tweets').empty();
@@ -44,7 +44,7 @@ const renderTweets = function(tweets) {
   }
 };
 
-//Loads all tweets stored at /tweets/
+// Loads all tweets stored at /tweets/
 const loadTweets = function() {
   $.ajax('/tweets/')
     .then(function(tweets) {
@@ -52,8 +52,26 @@ const loadTweets = function() {
     });
 };
 
-//Manages error within new tweet form
-const errorManager = function(contents) {
+// Opens/closes compose tweet viewer
+const useComposeTweetButton = function() {
+  if ($(".new-tweet").is(':visible')) $(".new-tweet").slideUp(500);
+  else {
+    $(".new-tweet").slideDown(500);
+    $("#tweet-text").focus();
+  }
+}
+
+// Alters character counter in new tweet form based on tweet text contents
+const composerCharCounter = function() {
+  const left = 140 - $(this).val().length;
+  const counter = $(this).parent().find('.counter');
+  counter.val(left);
+  if (left < 0) $(counter).css('color', 'red');
+  else $(counter).css('color', '#545149');
+}
+
+// Manages error within new tweet form
+const manageErrors = function(contents) {
   let error = "";
   if (contents === "") error = '⚠️ Tweet is too short ⚠️';
   else if (contents.length > 140) error = '⚠️ Tweet is too long ⚠️';
@@ -67,13 +85,36 @@ const errorManager = function(contents) {
   return error;
 }
 
-//Alters the compose tweet character counter based on length of tweet
-const composerCharCounter = function() {
-  const left = 140 - $(this).val().length;
-  const counter = $(this).parent().find('.counter');
-  counter.val(left);
-  if (left < 0) $(counter).css('color', 'red');
-  else $(counter).css('color', '#545149');
+// Deals with submitted text in new tweet box
+const newTweetSubmit = function (event) {
+  event.preventDefault();
+  const tweet = $('#new-tweet-form').serialize();
+  const contents = decodeURI(tweet.slice(5));
+  if(!manageErrors(contents)) {
+    $("#new-tweet-form")[0].reset();
+    $.post('/tweets/', tweet)
+      .then(() => loadTweets());
+  }
 }
 
-export { loadTweets, errorManager, composerCharCounter };
+// Brings viewport to top and opens compose tweet viewer if closed
+const useTopButton = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  if (!$(".new-tweet").is(':visible')) $(".new-tweet").slideDown(500);
+  $("#tweet-text").focus();
+}
+
+// Deals with compose tweet and top buttons based on scroll of window
+const alterButtons = function() {
+  const pageOffset = document.documentElement.scrollTop || document.body.scrollTop;
+  if (pageOffset > 100) {
+    $("#top").show();
+    $("#write-tweet").hide();
+  } else {
+    $("#top").hide();
+    $("#write-tweet").show();
+  }
+};
+
+export { loadTweets, useComposeTweetButton, composerCharCounter, newTweetSubmit, useTopButton, alterButtons };
